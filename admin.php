6c3936135709f0c1d433931c87c02c21d3fe1599
TEJVEER <tejveersharma384@gmail.com>
2021-03-20 17:35:11 +0530

    admin change password restored

--- admin.php
+++ admin.php
@@ -74,9 +74,42 @@ var password = document.myform.UserPassword.value;
           </div>
           <div class="row" >
             <div class="col-lg-12" style="font-size:20px;background-color: #00bfa6;height:300px">
-              <button type="button" name = "logout" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal"><span class="glyphicon glyphicon-pencil"></span> 
-Change Password
-</button>
+              <p>  <a href="#changePassword" data-toggle ="modal"> <button type="submit"  class="btn btn-danger" style="color:white" name= "change_password"><span class="glyphicon glyphicon-off"></span>  Change Password</button></a></p>
+                <!-- Button trigger modal -->
+
+<div class="modal fade" id="changePassword" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+       <form action="HomeDatabase.php" name = "myForm" method="post" onsubmit="return PasswordValidation()">
+  
+           <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" name="UserPassword" class="form-control" id="exampleInputPassword1" placeholder="New Password" required>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Confirm Password</label>
+    <input type="password" name="ConfirmPassword" class="form-control" id="exampleInputPassword2" placeholder="Confirm Password" required>
+  </div>
+ 
+  <button type="submit" name = "SubmitP" class="btn btn-primary">Change</button>
+</form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+                
+                
                 <p>  <a href="logout.php"> <button type="submit"  class="btn btn-danger" style="color:white" name= "logout"><span class="glyphicon glyphicon-off"></span>  logout</button></a></p>
               
                
@@ -214,40 +247,7 @@ else
       </div>
     </div>
       
-<!--Change password -->
-      
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Change Password </h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-       <form action="HomeDatabase.php" name = "myForm" method="post" onsubmit="return PasswordValidation()">
-  
-           <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" name="UserPassword" class="form-control" id="exampleInputPassword1" placeholder="New Password" required>
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Confirm Password</label>
-    <input type="password" name="ConfirmPassword" class="form-control" id="exampleInputPassword2" placeholder="Confirm Password" required>
-  </div>
- 
-  <button type="submit" name = "SubmitP" class="btn btn-primary">Change</button>
-</form>
-      </div>
-      <div class="modal-footer">
-        <button type="button"  class="btn btn-secondary" data-dismiss="modal">Close</button>
-        
-      </div>
-    </div>
-  </div>
-</div>
+
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
