11eba32cf59e635ebb66d5e2b5954a92cdd4c18b
TEJVEER <tejveersharma384@gmail.com>
2021-03-24 15:12:07 +0530

    auto fade out Login message

--- User.php
+++ User.php
@@ -33,6 +33,7 @@ if ($conn->connect_error) {
   
 
     <!-- Bootstrap CSS -->
+      
    <link href="css/bootstrap.min.css" rel="stylesheet"/>
        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 	<script src="jquery.min.js"></script>
@@ -90,12 +91,7 @@ if (name==null || name==""){
   <body>
        
     <div class="container" style="margin-top:5px;">
-        <?php 
-        if(isset($_SESSION["login_true"])){
-            echo '<h3 style = "font-weight: bold;color: green">Login successfully....</h3>';
-            unset($_SESSION["login_true"]);
-        }
-    ?>
+       
       <div class="row">
         <div class="col-lg-2" style="background-color: green;">
           <div class="row">
@@ -276,6 +272,21 @@ if (name==null || name==""){
   </ol>
 </nav>
             
+            
+            <?php 
+        if(isset($_SESSION["login_true"])){
+            ?><div id = "loginSuccess"><h3 style = "font-weight: bold;color: green">Login successfully....</h3></div> <?php
+            unset($_SESSION["login_true"]);
+        }
+    ?>
+        <script>
+        setTimeout(function() {
+    $('#loginSuccess').fadeOut('slow');
+}, 3000); // <-- time in milliseconds
+        </script>    
+    
+            
+            
             <div class="row" style="align-content:center;">
             <div class="col-lg-5" style="background-color: TOMATO; height:100px"><a data-toggle="modal" href="#profileModal" style="color:white;text-decoration: none;"><h3><span class="glyphicon glyphicon-user"></span>  Profile</h3></a></div>
                 
@@ -454,6 +465,9 @@ echo "</table>";
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   <script src="index.js"></script>
